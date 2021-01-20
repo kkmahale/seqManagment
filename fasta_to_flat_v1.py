aaf24f63954e9d7af5ca3fa5d0b15cd9f1c18231
kkmahale <48349003+kkmahale@users.noreply.github.com>
2021-01-20 17:12:35 +0530

    Convert fasta sequence to flat sequence
    Converts sequence in multiple lines in a fasta entry to a sequence in a single line. The header is retained as it is.

---
+++ fasta_to_flat_v1.py
@@ -0,0 +1,21 @@
+from string import *
+
+print 'The script and input file should be in the same folder. Output will be created at the same location as that of the script.\n'
+fname = raw_input('Please enter the name of the fasta file: ')
+ifile = open(fname, 'r')
+itext = ifile.read().split('>')
+ifile.close()
+while '' in itext:
+	itext.remove('')
+
+outfile = open('flat_' + fname, 'w')
+icount = 0
+for i in itext:
+	icount += 1
+	i1 = i.split('\n')
+	acc = i1[0]
+	iseq = ''.join(i1[1:])
+	outfile.write('>' + acc + '\n' + iseq + '\n')
+outfile.close()
+
+print 'Output file:', ('flat_' + fname), 'created successfully with', icount, 'entries.'
